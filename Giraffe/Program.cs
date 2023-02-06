aeeb53d7d5480919deb65708a06b6e3bd07a0bae
soontobemil <yourthought2020@gmail.com>
2023-02-05 20:54:34 -0800

    C# first lesson

---
+++ Giraffe/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35; 
+
+            Console.WriteLine("My name is " + characterName);
+            Console.WriteLine("Age is" + characterAge + "yes lol");
+            Console.WriteLine(" /");
+            Console.WriteLine("/");
+
+            Console.ReadLine();
+        }
+    }
+}
