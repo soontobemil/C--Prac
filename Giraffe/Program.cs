811fb18dc0ea9d5a33cf5b9001dca849de386dd6
soontobemil <yourthought2020@gmail.com>
2023-02-07 19:47:58 -0800

    2nd

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -8,15 +8,44 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            string characterName = "John";
-            int characterAge;
-            characterAge = 35; 
 
-            Console.WriteLine("My name is " + characterName);
-            Console.WriteLine("Age is" + characterAge + "yes lol");
-            Console.WriteLine(" /");
-            Console.WriteLine("/");
+            //first class
 
+            //string characterName = "John";
+            //int characterAge;
+            //characterAge = 35; 
+            //Console.WriteLine("My name is " + characterName);
+            //Console.WriteLine("Age is" + characterAge + " years old");
+            //Console.WriteLine(" /");
+            //Console.WriteLine("/");
+
+            //core data types
+
+            //string phrase = "Jay's Academy";
+            //char grade = 'A'; // character, only can put 1 word with '
+            //int age = 30; // no quotation mark needed
+            ////float, double, decimal // decimal points can be stored >>>
+            //double gpa = 4.3;
+            //bool isGeninus = true; //true or false 
+
+            //Strings
+            //Console.WriteLine("write\n write please");
+            //Console.WriteLine("\"write\" write please");
+            //string coding = "coding" + "isfun";
+            //Console.WriteLine(coding.Length); // get the length of the string
+            //Console.WriteLine( coding.ToUpper() );
+            //Console.WriteLine( coding.Contains("Academy") );
+            //Console.WriteLine(coding[0]);
+            //Console.WriteLine(coding.IndexOf("is"));
+            //Console.WriteLine(coding.Substring(8,3));
+
+
+            //Console.WriteLine("?");
+            //Console.WriteLine( Math.Abs(-40) );
+            //
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + name);
             Console.ReadLine();
         }
     }
