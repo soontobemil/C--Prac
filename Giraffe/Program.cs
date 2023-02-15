05979c7cab4ba82211702c7bb16df4f8abf20db5
Jay Sim <jay.sim@vshn.net>
2023-02-15 13:45:54 -0800

    Feb 15th

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -43,10 +43,52 @@ namespace MyApp // Note: actual namespace depends on the project name.
             //Console.WriteLine("?");
             //Console.WriteLine( Math.Abs(-40) );
             //
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello" + name);
+
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello" + name);
+            //Console.ReadLine();
+
+            //int num = Convert.ToInt32("48");
+            //Console.WriteLine(num + 2);
+            //Console.Write("Enter a number ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            // Console.WriteLine(num1);    
+            //string color;
+
+            //Console.Write("Enter a color: ");
+            //color = Console.ReadLine();
+            //Console.WriteLine("Roses are " + color);
+
+            //array
+            //int[] luckyNumbers = { 4, 8, 15, 23, 49, 122};
+            //luckyNumbers[0] = 9000;
+            //string[] friends = new string[10];
+            //friends[0] = "Jay";
+            //Console.WriteLine(luckyNumbers[0]);
+
+            //method
+
+            //    SayHi("Mike", 55);
+            //    SayHi("Jay", 25);
+            //    Console.ReadLine();
+            //}
+
+            //static void SayHi(string name, int age)
+            //{
+            //    Console.WriteLine("Hello " + name  + age);
+            //}
+
+            int cubenumber = cube(5);
+            Console.WriteLine(cubenumber);
             Console.ReadLine();
+
+        }
+
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
         }
     }
 }
