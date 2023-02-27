93a40dc5940f09ad1cc06b3e817fff6b7dd7330f
Jay Sim <jay.sim@vshn.net>
2023-02-26 21:10:52 -0800

    secure class

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -7,11 +7,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            book1.title = "Harry Potter";
-            book1.author = "JK Rowling";
-            book1.pages = 400;
-
+            Book book1 = new Book("Harry Potter","Jay SIm", 5000049, "dog");
+            Book book2 = new Book("Harry Potter2", "Jay SIM", 5000050, "Good");
+            Console.WriteLine(book1.HasHonors());
+            Console.WriteLine(book1.Rating);
+            Console.WriteLine(book2.Rating);
             Console.WriteLine(book1.title);
             Console.ReadLine();
         }
--- Giraffe/book.cs
+++ Giraffe/book.cs
@@ -6,9 +6,42 @@ namespace Giraffe
         public string title;
         public string author;
         public int pages;
+        private string rating;
 
-        public Book()
+        public Book ()
         {
+
+        }
+        public Book(string aTitle, string aAuthor, int aPages, string aRating)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+            Rating = aRating;
+        }
+
+        public bool HasHonors()
+        {
+            if(pages >= 100)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set {
+                if (value == "G" || value == "PG" || value == "Good")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "N/A";
+                }
+            }
         }
     }
 }
